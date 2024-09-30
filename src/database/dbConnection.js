f267305d7d95e4d1d2ae97cc4e973dee9295c9ca
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2024-10-01 03:07:54 +0700

    feat: add dependency and test query

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -1,4 +1,7 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
--- src/server.js
+++ src/server.js
@@ -11,8 +11,19 @@ fastify.get("/", function (request, reply) {
   reply.send({ hello: "world" });
 });
 
-fastify.get("/tt", function (request, reply) {
-  reply.send({ hello: "test" });
+fastify.get("/test", async (request, reply) => {
+  try {
+    const [rows] = await pool.execute("SELECT * FROM CODE_EPI_VACCINETYPE");
+
+    if (rows.length === 0) {
+      return reply.status(404).send({ message: "User not found" });
+    }
+
+    return rows[0];
+  } catch (err) {
+    fastify.log.error(err);
+    return reply.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 const start = async () => {
