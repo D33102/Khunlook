49834acf607f5862e29cfff408269bfab3970196
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2024-10-02 03:50:10 +0700

    fix: docker

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -6,6 +6,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
+  port: process.env.MYSQL_PORT,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   waitforconnections: true,
