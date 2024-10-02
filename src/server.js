8df65666500b310272aab2c1287b65b396315251
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2024-10-02 13:12:53 +0700

    feat: add swagger

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import Fastify from "fastify";
 import pool from "./database/dbConnection.js";
 import dotenv from "dotenv";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import { testRoute } from "./routes/test.js";
+import { swaggerOptions, swaggerUiOptions } from "./utils/swagger.js";
 
 dotenv.config();
 
@@ -9,8 +12,14 @@ const fastify = Fastify({
   logger: true,
 });
 
+// Swagger
+fastify.register(fastifySwagger, swaggerOptions);
+fastify.register(fastifySwaggerUi, swaggerUiOptions);
+
+// Db connection
 fastify.decorate("mysql", pool);
 
+// Routes
 fastify.register(testRoute, { prefix: "/" });
 
 const start = async () => {
