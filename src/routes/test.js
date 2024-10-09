7523d2c239469053f063d4ec016e2cf3c07678c8
PwFaze <feze2013@hotmail.com>
2024-10-09 14:27:07 +0700

    feat: test route now require token from login

--- src/routes/test.js
+++ src/routes/test.js
@@ -3,7 +3,14 @@ import { testController } from "../controllers/test.js";
 export function testRoute(fastify, options, done) {
   fastify.get("/", testController.testGreet);
 
-  fastify.get("/test", testController.testHand);
+  fastify.get(
+    "/test",
+    {
+      preValidation: [fastify.authenticate],
+      schema: testController.testHand.schema,
+    },
+    testController.testHand.handler
+  );
 
   done();
 }
