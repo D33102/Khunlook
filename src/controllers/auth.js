1310a2aaf29a8de9bfd5961f75321db3e5d375b7
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2024-10-15 23:30:02 +0700

    feat: hash password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,5 @@
 import { authSchema } from "../models/auth.js";
+import bcrypt from "bcrypt";
 
 export const authController = {
   userLogin: {
@@ -6,28 +7,39 @@ export const authController = {
     handler: async (request, reply) => {
       try {
         const { USERNAME, PASSWORD } = request.body;
+
         const [rows] = await request.server.mysql.execute(
-          "SELECT * FROM USER WHERE `USERNAME` = ? AND `PASSWORD` = ?",
-          [USERNAME, PASSWORD]
+          "SELECT * FROM USER WHERE `USERNAME` = ?",
+          [USERNAME]
         );
+
         if (rows.length === 0) {
           return reply
             .status(401)
             .send({ error: "Invalid username or password" });
         }
+
+        const user = rows[0];
+
+        const isPasswordValid = await bcrypt.compare(PASSWORD, user.PASSWORD);
+        if (!isPasswordValid) {
+          return reply
+            .status(401)
+            .send({ error: "Invalid username or password" });
+        }
+
         const accessToken = request.server.jwt.sign(
           { username: USERNAME },
-          { expiresIn: "1hr" } // Access token expires in 15 minutes
+          { expiresIn: "1hr" } // Access token expires in 1 hour
         );
 
         // Generate refresh token
         const refreshToken = request.server.jwt.sign(
           { username: USERNAME },
-          { expiresIn: "12hr" } // Refresh token expires in 7 days
+          { expiresIn: "12hr" } // Refresh token expires in 12 hours
         );
-        return reply
-          .status(200)
-          .send({ accessToken: accessToken, refreshToken: refreshToken });
+
+        return reply.status(200).send({ accessToken, refreshToken });
       } catch (err) {
         request.server.log.error(err);
         return reply.status(500).send({ message: "Internal Server Error" });
@@ -41,6 +53,7 @@ export const authController = {
       if (!refreshToken) {
         return reply.status(403).send({ error: "Refresh Token is required" });
       }
+
       try {
         const decoded = request.server.jwt.verify(refreshToken);
 
