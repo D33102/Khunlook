60b399963adb072bfb2b9f7b53d4767da57db664
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2024-12-07 21:56:13 +0700

    feat: add validation

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,13 @@ import { userController } from "../controllers/user.js";
 
 export function userRoute(fastify, options, done) {
   fastify.post("/", userController.userRegister);
-  fastify.get("/:id", userController.getUserChildren)
+  fastify.get(
+    "/:id",
+    {
+      preValidation: [fastify.authenticate],
+      schema: userController.getUserChildren.schema,
+    },
+    userController.getUserChildren.handler
+  );
   done();
 }
