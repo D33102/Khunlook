7e7a679e2820e76fa2543b5edae8b4706ea06ffe
Vorraphan Treepoonsap <vorraphant@gmail.com>
2024-12-12 13:38:38 +0700

    userRoute fix

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,9 @@ import { userController } from "../controllers/user.js";
 import { userSchema } from "../models/user.js";
 
 export function userRoute(fastify, options, done) {
-  fastify.post("/", userController.userRegister);
+  fastify.post("/", 
+    {schema: userController.userRegister.schema},
+    userController.userRegister);
   
   done();
 }
