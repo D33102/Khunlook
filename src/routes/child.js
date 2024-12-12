ea9b23d010a913f5e3353389e938b61af110e2a5
Vorraphan Treepoonsap <vorraphant@gmail.com>
2024-12-12 13:08:24 +0700

    fix route child

--- src/routes/child.js
+++ src/routes/child.js
@@ -1,6 +1,14 @@
 import { childController } from "../controllers/auth.js";
 import { childSchema } from "../models/child.js";
 export function childRoute(fastify, options, done) {
+  fastify.get(
+    "/:user_id",
+    {
+      preValidation: [fastify.authenticate],
+      schema: userController.getChildren.schema,
+    },
+    childController.getChild.handler
+  );
   fastify.post("/", {
     preValidation: [fastify.authenticate],
     schema: childSchema.addChildSchema,
