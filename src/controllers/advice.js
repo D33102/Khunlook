14ee6393d3f3a7be27b86a5f49cd2dde5fa81b07
Vorraphan Treepoonsap <vorraphant@gmail.com>
2025-02-07 00:15:24 +0700

    feat: get proper advice

---
+++ src/controllers/advice.js
@@ -0,0 +1,41 @@
+import { adviceSchema } from "../models/advice.js";
+
+export const adviceController = {
+  getProperAdvice: {
+    schema: adviceSchema.getProperAdvice,
+    handler: async (request, reply) => {
+        try{
+            const {currentAge} = request.body
+            const [rows] = await request.server.mysql.execute(
+                `
+                SELECT CODE, AGE_MONTH_DESCRIPTION, INFORMATION 
+                FROM GL_NUTRITION 
+                WHERE ? >= MIN_AGE_MONTH && ? <= MAX_AGE_MONTH`,
+                [parseInt(currentAge), parseInt(currentAge)]
+              );
+              if (rows.length == 0){
+                return reply.status(404).send({
+                    success:false,
+                    message:"query not found",
+                    error: "query not found"
+                })
+              }
+              console.log(rows)
+              return reply.status(200).send({
+                success:true,
+                message:"query proper advice success",
+                data: rows
+              })
+
+        }
+        catch(err){
+            request.server.log.error(err);
+            return reply.status(500).send({
+            success: false,
+            message: "Internal Server Error",
+            error: err.message ||"An unexpected error occurred. Please try again later.",
+            });
+        }
+    },
+  },
+}
