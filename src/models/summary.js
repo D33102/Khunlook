fca798c9de0fb9beeb1ec4c33c739640b197ec4f
AimChayakorn <79163250+AimChayakorn@users.noreply.github.com>
2025-04-17 21:58:41 +0700

    Summary info get info (#23)
    * feat: implement summary info get info
    
    * chore: remove log

--- src/models/summary.js
+++ src/models/summary.js
@@ -41,4 +41,61 @@ export const summarySchema = {
       },
     },
   },
+  getInfoSchema: {
+    description: "Get summary information",
+    tags: ["Summary"],
+    body: {
+      type: "object",
+      properties: {
+        query_childpid: { type: "string" },
+      },
+      required: ["query_childpid"],
+    },
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          success: { type: "boolean" },
+          content: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                NAME: { type: "string" },
+                SEX: { type: "string" },
+                BIRTH: { type: "string", format: "date-time" },
+                MOTHER: { type: "string" },
+                PID: { type: "string" },
+                IN_PLAN: { type: "string" },
+                VACCINETYPE: { type: "string" },
+                DATE_SERV: { type: "string", format: "date-time" },
+                CODE: { type: "string" },
+                WEB_GRP_NAME: { type: "string" },
+                WEB_GRP_ORDER: { type: "integer" },
+                GRP_NAME: { type: "string" },
+                DESCRIPTION_TH: { type: "string" },
+                DESCRIPTION_TABLE: { type: "string" },
+                AGE: { type: "integer" },
+                AGE_MAX: { type: "integer" },
+                HOSPITAL: { type: "string" },
+                HOSPITALCODE: { type: "string" },
+                AGE_MONTH: { type: "integer" },
+              },
+              required: ["NAME", "SEX", "BIRTH", "MOTHER", "PID"],
+            },
+          },
+        },
+        required: ["success", "content"],
+      },
+      500: {
+        type: "object",
+        properties: {
+          success: { type: "boolean" },
+          message: { type: "string" },
+          error: { type: "string" },
+        },
+        required: ["success", "message", "error"],
+      },
+    },
+  },
 };
